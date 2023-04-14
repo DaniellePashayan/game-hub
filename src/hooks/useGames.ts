6082c9f16c3f596bdcc66c1d3756c7a1a9366a17
DaniellePashayan <daniellepashayan@gmail.com>
2023-04-14 17:27:59 -0400

    restructured interfaces

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,16 +2,17 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-const useGames = () => {
-  interface Game {
-    id: number;
-    name: string;
-  }
+export interface Game {
+  id: number;
+  name: string;
+}
 
-  interface FetchGamesResposne {
-    count: number;
-    results: Game[];
-  }
+interface FetchGamesResposne {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
 
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
