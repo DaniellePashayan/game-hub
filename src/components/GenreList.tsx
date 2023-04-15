2289718baf3df3d9d91fa4e4e72b6ee460289004
DaniellePashayan <daniellepashayan@gmail.com>
2023-04-14 20:10:54 -0400

    highlight selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,10 +3,11 @@ import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
-  onSelectGenre: (genre:Genre) => void
+  onSelectGenre: (genre:Genre) => void;
+  selectedGenre: Genre | null
 }
 
-const GenreList = ({onSelectGenre}:Props) => {
+const GenreList = ({onSelectGenre, selectedGenre}:Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -23,6 +24,7 @@ const GenreList = ({onSelectGenre}:Props) => {
               src={getCroppedImageUrl(genre.image_background)}
             />
             <Button 
+            fontWeight={genre.id === selectedGenre?.id ? 'bold': 'normal'}
             onClick={() => onSelectGenre(genre)}
             variant='link' 
             fontSize={"md"}>{genre.name}</Button>
