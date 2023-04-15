e3e55cae6be85deaae19dd257ff5e12c0dc7b4ec
DaniellePashayan <daniellepashayan@gmail.com>
2023-04-14 22:03:46 -0400

    handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,11 +1,12 @@
+import defaultImage from '../assets/no-image-placeholder-6f3882e0.webp'
+
 const getCroppedImageUrl = (url: string) => {
   //this API support cropping the image via the URL by adding 'crop' to the URL itself with dimensions
-  if (!url) return "";
+  if (!url) return defaultImage;
 
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
 
-//TODO: some games dont have images, need to render skeleton image
 export default getCroppedImageUrl;
